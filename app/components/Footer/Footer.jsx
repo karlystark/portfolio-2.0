a10e72e19d1ee5ede9c430472d36d7b6296a80e0
Karly Stark <karlystark@gmail.com>
2025-09-20 17:50:01 -0700

    footer styling light mode

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="bg-[#222222] text-[#f3f3f2] py-8 sm:py-12 px-4 z-30">
+    <footer className="text-[#171717] py-8 sm:py-12 px-4 z-30">
       <div className="mx-auto">
 
         {/* Bottom bar */}
-        <div className="border-t border-[#f3f3f2] text-[#f3f3f2] pt-4 sm:pt-6 flex flex-col sm:flex-row justify-between items-center space-y-3 sm:space-y-0">
+        <div className="border-t border-[#171717] text-[#171717] pt-4 sm:pt-6 flex flex-col sm:flex-row justify-between items-center space-y-3 sm:space-y-0">
           <p className="text-xs sm:text-sm">
             &copy; 2025 k.stark
           </p>
