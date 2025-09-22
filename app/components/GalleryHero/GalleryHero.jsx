67fe5a486a9c677f49e852d249451262fb780c20
Karly Stark <karlystark@gmail.com>
2025-09-22 16:00:18 -0700

    refactoring homepage components and rendering based on mobile/desktop

---
+++ app/components/GalleryHero/GalleryHero.jsx
@@ -0,0 +1,66 @@
+'use client';
+
+import Gallery from '../Gallery/Gallery';
+import { useEffect } from 'react';
+import { useSpring } from 'framer-motion';
+import styles from '../../page.module.css';
+import Lenis from 'lenis';
+
+const projects = [
+  {
+    name: "1",
+    handle: "1"
+  },
+  {
+    name: "2",
+    handle: "2"
+  },
+  {
+    name: "3",
+    handle: "3"
+  },
+];
+
+export default function GalleryHero() {
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
+  const spring = {
+    stiffness: 150,
+    damping: 15,
+    mass: 0.1
+  };
+
+  const mousePosition = {
+    x: useSpring(0, spring),
+    y: useSpring(0, spring)
+  };
+
+  const mouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const targetX = clientX - (window.innerWidth / 2 * 0.25);
+    const targetY = clientY - (window.innerWidth / 2 * 0.30);
+
+    mousePosition.x.set(targetX);
+    mousePosition.y.set(targetY);
+  };
+
+  return (
+    <main onMouseMove={mouseMove} className={styles.main}>
+      {
+        projects.map(({ handle }, i) => {
+          return <Gallery mousePosition={mousePosition} handle={handle} key={i} />;
+        })
+      }
+    </main>
+
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,66 +1,23 @@
 'use client';
 
-import Gallery from './components/Gallery/Gallery';
-import { useEffect } from 'react';
-import { useSpring } from 'framer-motion';
-import styles from './page.module.css';
-import Lenis from 'lenis';
-// import DescriptionList from './components/DescriptionList/DescriptionList';
+import GalleryHero from './components/GalleryHero/GalleryHero';
+import DescriptionList from './components/DescriptionList/DescriptionList';
+import { useState, useEffect } from 'react';
 
-const projects = [
-  {
-    name: "1",
-    handle: "1"
-  },
-  {
-    name: "2",
-    handle: "2"
-  },
-  {
-    name: "3",
-    handle: "3"
-  },
-];
 
 export default function Home() {
-  useEffect(() => {
-    const lenis = new Lenis();
+  const [isMobile, setIsMobile] = useState(false);
 
-    function raf(time){
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf)
+  useEffect(() => {
+    // Function to check window width
+    const handleResize = () => setIsMobile(window.innerWidth < 639);
+
+    // Run at start + on resize
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const spring = {
-    stiffness: 150,
-    damping: 15,
-    mass: 0.1
-  }
-
-  const mousePosition = {
-    x: useSpring(0, spring),
-    y: useSpring(0, spring)
-  }
-
-  const mouseMove = (e) => {
-    const { clientX, clientY } = e;
-    const targetX = clientX - (window.innerWidth / 2 * 0.25);
-    const targetY = clientY - (window.innerWidth / 2 * 0.30);
-
-    mousePosition.x.set(targetX);
-    mousePosition.y.set(targetY);
-  }
-
-  return (
-    <main onMouseMove={mouseMove} className={styles.main}>
-      {
-        projects.map(({handle}, i) => {
-          return <Gallery mousePosition={mousePosition} handle={handle} key={i} />
-        })
-      }
-    </main>
-
-  );
+  return isMobile ? <DescriptionList /> : <GalleryHero />;
 }
